606e4422acc4bba57722f566854c51b4032fcbeb
faridraka <faridrakapratama@gmail.com>
2026-06-19 00:43:02 +0700

    setup: folder structure, color system, and js

---
+++ script.js
@@ -0,0 +1,210 @@
+const menuItems = [
+  {
+    name: "Nasi Putih",
+    category: "Pokok",
+    kkal: 180,
+    price: 5000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Nasi Goreng Spesial",
+    category: "Pokok",
+    kkal: 450,
+    price: 18000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Mie Goreng Jawa",
+    category: "Pokok",
+    kkal: 380,
+    price: 16000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Spaghetti Aglio Olio",
+    category: "Pokok",
+    kkal: 420,
+    price: 28000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Kentang Panggang",
+    category: "Pokok",
+    kkal: 220,
+    price: 15000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Roti Gandum (2 lembar)",
+    category: "Pokok",
+    kkal: 160,
+    price: 8000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Ayam Goreng Tepung",
+    category: "Lauk-Pauk",
+    kkal: 300,
+    price: 17000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Ayam Bakar Madu",
+    category: "Lauk-Pauk",
+    kkal: 280,
+    price: 20000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Rendang Daging Sapi",
+    category: "Lauk-Pauk",
+    kkal: 380,
+    price: 32000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Tempe Orek",
+    category: "Lauk-Pauk",
+    kkal: 150,
+    price: 8000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Tahu Goreng",
+    category: "Lauk-Pauk",
+    kkal: 120,
+    price: 6000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Ikan Nila Bakar",
+    category: "Lauk-Pauk",
+    kkal: 250,
+    price: 22000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Telur Dadar",
+    category: "Lauk-Pauk",
+    kkal: 190,
+    price: 7000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Beef Steak Sirloin",
+    category: "Lauk-Pauk",
+    kkal: 420,
+    price: 45000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Tumis Kangkung",
+    category: "Sayur",
+    kkal: 90,
+    price: 9000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Sayur Asem",
+    category: "Sayur",
+    kkal: 70,
+    price: 8000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Capcay Kuah",
+    category: "Sayur",
+    kkal: 110,
+    price: 13000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Salad Sayur Segar",
+    category: "Sayur",
+    kkal: 80,
+    price: 14000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Gado-Gado",
+    category: "Sayur",
+    kkal: 280,
+    price: 16000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Pisang (1 buah)",
+    category: "Buah",
+    kkal: 105,
+    price: 5000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Apel Fuji (1 buah)",
+    category: "Buah",
+    kkal: 95,
+    price: 7000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Semangka (1 potong)",
+    category: "Buah",
+    kkal: 60,
+    price: 6000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Fruit Salad Cup",
+    category: "Buah",
+    kkal: 140,
+    price: 17000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Es Teh Manis",
+    category: "Minuman",
+    kkal: 90,
+    price: 5000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Air Mineral 600ml",
+    category: "Minuman",
+    kkal: 0,
+    price: 4000,
+    isChecklist: false,
+    image: ""
+  },
+  {
+    name: "Jus Alpukat",
+    category: "Minuman",
+    kkal: 230,
+    price: 18000,
+    isChecklist: false,
+    image: ""
+  }
+];
